d80527779f8e78aa2667ac59dc089012843f00ec
DanielaMazur <danielamazur2000@gmail.com>
2021-09-19 20:29:09 +0300

    LAB-2 add possibility to check items

--- CS/MainWindow.xaml.cs
+++ CS/MainWindow.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.Win32;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace CS
 {
@@ -12,11 +17,12 @@ namespace CS
      public partial class MainWindow : Window
      {
           private string _importedFileContents;
-          private List<Dictionary<string, string>> CutsomItemsDictList { get; set; }
+          private List<CustomItem> CustomItemsList;
+
           public MainWindow()
           {
                InitializeComponent();
-               Resources["CutsomItemsDictList"] = new List<Dictionary<string, string>>();
+               Resources["CutsomItemsDictList"] = new List<CustomItem>();
           }
 
           private void OpenAuditFile(object sender, RoutedEventArgs e)
@@ -31,8 +37,13 @@ namespace CS
                     {
                          using var sr = new StreamReader(fileDialog.FileName);
                          _importedFileContents = sr.ReadToEnd();
-                         CutsomItemsDictList = this.ConvertToDict(_importedFileContents);
-                         Resources["CutsomItemsDictList"] = CutsomItemsDictList;
+                         CustomItemsList = this.ConvertToDict(_importedFileContents);
+                         Resources["CustomItemsDictList"] = CustomItemsList;
+                         if(CustomItemsList.Count > 0)
+                         {
+                              var BodyContainer = (Grid)this.FindName("BodyContainer");
+                              BodyContainer.Visibility = Visibility.Visible;
+                         }
                     }
                     catch (IOException error)
                     {
@@ -53,14 +64,42 @@ namespace CS
           private void SaveJSONFile(object sender, RoutedEventArgs e)
           {
                SaveFileDialog saveFileDialog = new();
-               var JSONFileData = Newtonsoft.Json.JsonConvert.SerializeObject(CutsomItemsDictList);
+               var JSONFileData = JsonConvert.SerializeObject(CustomItemsList.Select(item => item.Properties));
                if (saveFileDialog.ShowDialog() == true)
                {
                     File.WriteAllText(saveFileDialog.FileName + ".json", JSONFileData);
                }
           }
 
-          private List<Dictionary<string, string>> ConvertToDict(string fileData)
+          private void HandleAllItemsCheck(object sender, RoutedEventArgs e)
+          {
+               foreach (var customItem in CustomItemsList)
+               {
+                    customItem.IsChecked = true;
+               }
+          }
+
+          private void HandleAllItemsUnchecked(object sender, RoutedEventArgs e)
+          {
+               foreach (var customItem in CustomItemsList)
+               {
+                    customItem.IsChecked = false;
+               }
+          }
+
+          private void HandleCheckOneItem(object sender, RoutedEventArgs e)
+          {
+               var itemToCheck = CustomItemsList.Single(item => item.Id.ToString() == ((CheckBox)sender).Uid);
+               itemToCheck.IsChecked = true;
+          }
+
+          private void HandleUnCheckOneItem(object sender, RoutedEventArgs e)
+          {
+               var itemToUnCheck = CustomItemsList.Single(item => item.Id.ToString() == ((CheckBox)sender).Uid);
+               itemToUnCheck.IsChecked = false;
+          }
+
+          private List<CustomItem> ConvertToDict(string fileData)
           {
                var customItemPattern = @"<custom_item>([\S\s]*?)</custom_item>";
                var customItemKeyValuePattern = @"^[\s]+([a-z_]+)\s+:\s+((""[\S\s\n]*?"")|([^\n]+))";
@@ -70,7 +109,7 @@ namespace CS
 
                var customItems = regexCustomItemPattern.Matches(fileData);
 
-               var parsedCustomItems = new List<Dictionary<string, string>>();
+               var parsedCustomItems = new List<CustomItem>();
 
                foreach (var customItem in customItems)
                {
@@ -85,10 +124,42 @@ namespace CS
 
                          keysValuesDict.Add(key, value);
                     }
-                    parsedCustomItems.Add(keysValuesDict);
+                    var newCustomItem = new CustomItem(parsedCustomItems.Count, keysValuesDict);
+                    parsedCustomItems.Add(newCustomItem);
                }
 
                return parsedCustomItems;
+          }
+
+          private class CustomItem : INotifyPropertyChanged
+          {
+               public int Id { get; private set; }
+               private bool? _isChecked = false;
+               public bool? IsChecked
+               {
+                    get
+                    {
+                         return _isChecked;
+                    }
+                    set
+                    {
+                         _isChecked = value;
+                         NotifyPropertyChanged();
+                    }
+               }
+               public Dictionary<string, string> Properties { get; private set; }
+               public CustomItem(int id, Dictionary<string, string> properties)
+               {
+                    Id = id;
+                    Properties = properties;
+               }
+
+               public event PropertyChangedEventHandler PropertyChanged;
+               private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+               {
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+               }
+
           }
      }
 }
